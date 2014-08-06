d1fc3034622f0e4a2b8e821ce6d78d76da6846a7
Jivan Amara <Development@JivanAmara.net>
2014-08-06 13:19:47 -0700

    Added check for /etc/apt/apt.conf.d/10periodic to ensure unattended_upgrades   will run. Now displays of the number of days between unattended upgrades.

--- unattended_upgrades.py
+++ unattended_upgrades.py
@@ -35,6 +35,29 @@ def config_file_contains(config_filename, content_regex):
     return ret
 
 
+def get_config_value(config_filename, config_value_regex):
+    """
+        @since: 2014-08-06
+        @author: Jivan
+        @return: The value of the config variable described by \a config_value_regex.
+        @param config_value_regex: a regular expression which will match the config variable
+            and it's value as the first group.
+            Example: if a config variable looks like 'mysetting: <n>' config_value_regex would
+                be 'mysetting: (\d+)'
+    """
+    # config file object
+    cf = open(config_filename, 'r')
+    # file contents
+    fc = cf.read()
+    # search match
+    sm = re.search(config_value_regex, fc, flags=re.MULTILINE)
+    if not sm or len(sm.groups()) != 1:
+        ret = None
+    else:
+        ret = sm.group(1)
+    return ret
+
+
 try:
     # --- Check that 'update-notifier-common' is installed.
     #    This isn't really necessary on Ubuntu machines, but will remind Debian users that this
@@ -72,13 +95,33 @@ try:
         print("WARNING - 'unattended-upgrades' is not configured to install recommended updates")
         sys.exit(WARNING)
 
+    # --- Check that unattended-upgrades is configured to run.
+    config_variable_regexes = [
+        # Make sure this one is first
+        re.escape(r'APT::Periodic::Unattended-Upgrade "') + r'(\d+)' + re.escape(r'";'),
+        #
+        r'APT::Periodic::Update-Package-Lists "(\d+)";',
+        r'APT::Periodic::Download-Upgradeable-Packages "(\d+)";',
+        r'APT::Periodic::AutocleanInterval "(\d+)";',
+    ]
+    config_filename = '/etc/apt/apt.conf.d/10periodic'
+    for cvr in config_variable_regexes:
+        val = int(get_config_value(config_filename, cvr))
+        if not val:
+            print("CRITICAL - 'unattended-upgrades is not configured to run properly.\n"\
+                  "In {}: {} is set to {}".format(config_filename, cvr, val))
+            sys.exit(CRITICAL)
+        if cvr == config_variable_regexes[0]:
+            unattended_upgrade_period = val
+
+
     # --- Check if a reboot is required (by checking for file '/var/run/reboot-required')
     reboot_required = os.path.exists('/var/run/reboot-required')
     if reboot_required:
         print("WARNING - Server requires a reboot")
         sys.exit(WARNING)
     else:
-        print("OK - unattended_upgrades is running properly and no reboot is required")
+        print("OK - unattended_upgrades runs every {} days".format(unattended_upgrade_period))
         sys.exit(OK)
 except Exception as ex:
     print(ex)
